072df0e8955035b29722d1c2bb2280012113bc3a
MeharChoudhary <s533976@nwmissouri.edu>
2019-03-03 17:46:05 -0600

    update db Initializer

--- WebApplication2/WebApplication2/Data/DbInitializer.cs
+++ WebApplication2/WebApplication2/Data/DbInitializer.cs
@@ -34,14 +34,82 @@ namespace WebApplication2.Data
             }
 
 
+            
             //for Credit - Aakash
 
-            //for Student Id- Aakash
+            //for DegreeCredit - Mehar Choudhary
 
-            //for DegreeCredit - Dasari
+             if (context.DegreeCredits.Any())
+            {
+                Console.WriteLine("DegreeCredits already exist.");
+            }
+            else
+            {
+                var degreeCredits = new DegreeCredit[]
+                {
+                    new DegreeCredit {DegreeCreditID=1, DegreeID=1, CreditID=460},
+                    new DegreeCredit {DegreeCreditID=2, DegreeID=1, CreditID=356},
+                    new DegreeCredit {DegreeCreditID=3, DegreeID=1, CreditID=542},
+                    new DegreeCredit {DegreeCreditID=4, DegreeID=1, CreditID=563},
+                    new DegreeCredit {DegreeCreditID=5, DegreeID=1, CreditID=560},
+                    new DegreeCredit {DegreeCreditID=6, DegreeID=1, CreditID=664},
+                    new DegreeCredit {DegreeCreditID=7, DegreeID=1, CreditID=618},
+                    new DegreeCredit {DegreeCreditID=8, DegreeID=1, CreditID=555},
+                    new DegreeCredit {DegreeCreditID=9, DegreeID=1, CreditID=691},
+                    new DegreeCredit {DegreeCreditID=10, DegreeID=1, CreditID=692},
+                    new DegreeCredit {DegreeCreditID=11, DegreeID=1, CreditID=6},
+                    new DegreeCredit {DegreeCreditID=12, DegreeID=1, CreditID=10},
+                    new DegreeCredit {DegreeCreditID=13, DegreeID=1, CreditID=20},
+                    new DegreeCredit {DegreeCreditID=14, DegreeID=2, CreditID=460},
+                    new DegreeCredit {DegreeCreditID=15, DegreeID=2, CreditID=542},
+                    new DegreeCredit {DegreeCreditID=16, DegreeID=2, CreditID=563},
+                    new DegreeCredit {DegreeCreditID=17, DegreeID=2, CreditID=560},
+                    new DegreeCredit {DegreeCreditID=18, DegreeID=2, CreditID=664},
+                    new DegreeCredit {DegreeCreditID=19, DegreeID=2, CreditID=618},
+                    new DegreeCredit {DegreeCreditID=20, DegreeID=2, CreditID=555},
+                    new DegreeCredit {DegreeCreditID=21, DegreeID=2, CreditID=691},
+                    new DegreeCredit {DegreeCreditID=22, DegreeID=2, CreditID=692},
+                    new DegreeCredit {DegreeCreditID=23, DegreeID=2, CreditID=6},
+                    new DegreeCredit {DegreeCreditID=24, DegreeID=2, CreditID=10},
+                    new DegreeCredit {DegreeCreditID=25, DegreeID=2, CreditID=20},
+                    new DegreeCredit {DegreeCreditID=26, DegreeID=3, CreditID=356},
+                    new DegreeCredit {DegreeCreditID=27, DegreeID=3, CreditID=542},
+                    new DegreeCredit {DegreeCreditID=28, DegreeID=3, CreditID=563},
+                    new DegreeCredit {DegreeCreditID=29, DegreeID=3, CreditID=560},
+                    new DegreeCredit {DegreeCreditID=30, DegreeID=3, CreditID=664},
+                    new DegreeCredit {DegreeCreditID=31, DegreeID=3, CreditID=618},
+                    new DegreeCredit {DegreeCreditID=32, DegreeID=3, CreditID=555},
+                    new DegreeCredit {DegreeCreditID=33, DegreeID=3, CreditID=691},
+                    new DegreeCredit {DegreeCreditID=34, DegreeID=3, CreditID=692},
+                    new DegreeCredit {DegreeCreditID=35, DegreeID=3, CreditID=6},
+                    new DegreeCredit {DegreeCreditID=36, DegreeID=3, CreditID=10},
+                    new DegreeCredit {DegreeCreditID=37, DegreeID=3, CreditID=20},
+                    new DegreeCredit {DegreeCreditID=38, DegreeID=4, CreditID=542},
+                    new DegreeCredit {DegreeCreditID=39, DegreeID=4, CreditID=563},
+                    new DegreeCredit {DegreeCreditID=40, DegreeID=4, CreditID=560},
+                    new DegreeCredit {DegreeCreditID=41, DegreeID=4, CreditID=664},
+                    new DegreeCredit {DegreeCreditID=42, DegreeID=4, CreditID=618},
+                    new DegreeCredit {DegreeCreditID=43, DegreeID=4, CreditID=555},
+                    new DegreeCredit {DegreeCreditID=44, DegreeID=4, CreditID=691},
+                    new DegreeCredit {DegreeCreditID=45, DegreeID=4, CreditID=692},
+                    new DegreeCredit {DegreeCreditID=46, DegreeID=4, CreditID=6},
+                    new DegreeCredit {DegreeCreditID=47, DegreeID=4, CreditID=10},
+                    new DegreeCredit {DegreeCreditID=48, DegreeID=4, CreditID=20},
+
+                };
+                Console.WriteLine($"Inserted {degreeCredits.Length} new degree credits.");
+
+                foreach (DegreeCredit d in degreeCredits)
+                {
+                    context.DegreeCredits.Add(d);
+                }
+                context.SaveChanges();
+            }
+
 
             //for DegreePlan - Dasari
 
+            //for Student Id- Dasari
 
 
 
